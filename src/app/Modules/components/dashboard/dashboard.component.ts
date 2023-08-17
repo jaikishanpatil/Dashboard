02f35e16255ab90e3f8ee7bfe4184046c88b6d3b
jaikishanpatil <patiljaikishan4@gmail.com>
2023-08-17 17:33:20 +0530

    services added to new folder

--- src/app/Modules/components/dashboard/dashboard.component.ts
+++ src/app/Modules/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AppServiceService } from 'src/app/app-service.service';
+import { AppServiceService } from 'src/app/services/app-service.service';
 
 @Component({
   selector: 'app-dashboard',
